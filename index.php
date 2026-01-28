06a7532161faa0f59ae1ba290e38f529e972c5f9
Gilang737 <gs7832583@gmail.com>
2026-01-28 13:51:52 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, aspernatur non delectus at vitae aliquid necessitatibus obcaecati commodi voluptatem cupiditate nihil accusantium reprehenderit, voluptas maiores iste molestias nemo alias perspiciatis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
